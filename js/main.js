320c29efe5c82b9c0c0b6e9e103228ec111e6570
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-01-18 17:50:00 +0300

    Adaptive layout for mobile and small-width devices

--- js/main.js
+++ js/main.js
@@ -6,10 +6,9 @@
       // Finding the first element in the document with the given selector and attributing it to the variable
         window.onscroll = () => {
             // Giving the trigger for the following condition to check
-        if (window.pageYOffset > 100) {
-            // If the window scroll on Y axis (verticals) goes past 100px
-            // ! Decided to multiply the distance by 2 (initially 50px)
-                header.classList.add('header_active');
+        if (window.pageYOffset > 50) {
+            // If the window scroll on Y axis (verticals) goes past 50px
+            header.classList.add('header_active');
                 // Start using the property'header_active', which is set in CSS
          } else {
             header.classList.remove('header_active');
